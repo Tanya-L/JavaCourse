d85c9d10644669178d43fda6b021db65f4c97825
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-06-23 19:38:16 +0200

    done

--- src/tl222xy_assign1/fibonacci/Fibonacci.java
+++ src/tl222xy_assign1/fibonacci/Fibonacci.java
@@ -8,6 +8,31 @@ package tl222xy_assign1.fibonacci;
 public class Fibonacci {
 
     public static void main(String[] args) {
+        int maxNumber = 1000;
+        int previousNumber = 0;
+        int nextNumber = 1;
+        int oddSum = 0;
+        int oddCounter = 1;
 
+        System.out.print("Fibonacci sequence upto " + maxNumber + " : ");
+        // using WHILE loop, compare the sum of last two number with maximum number 1000 in this case.
+        // If next number is less or equals to maxNumber, should be print, else - stop.
+
+        while (true) {
+            int sum = previousNumber + nextNumber;
+            previousNumber = nextNumber;
+            nextNumber = sum;
+            if (nextNumber > maxNumber) {
+                break;
+            }
+            System.out.print(nextNumber + " ");
+
+            // Compute and print a sum of the odd-valued terms in the required Fibonacci sequence.
+            if (oddCounter % 2 == 1) {
+                oddSum += nextNumber;
+            }
+            oddCounter++;
+        }
+        System.out.print("\nSum of odd elements of Fibonacci sequence upto " + maxNumber + " : " + oddSum);
     }
 }
