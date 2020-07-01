0dc2bef6d82cfdaa46eebbc7a7d419e1e700e311
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-07-01 13:16:02 +0200

    First commit

---
+++ src/tl222xy_assign2_money/Money.java
@@ -0,0 +1,22 @@
+/**
+ * Money.java
+ *
+ * @author Tetiana Lytovchenko
+ */
+package tl222xy_assign2_money;
+
+/**
+ * Write a program Money.java that defines a class named Money whose objects represent
+ * amounts of U.S. money. The class shall have:
+ * - Two instance variables of type int for the dollars and cents in the amount of money.
+ * - A constructor with two parameters of type int for the dollars and cents,
+ * - Another constructor with one parameter of type int for an amount of dollars. It should set cents to zero
+ * - A no-argument constructor.
+ * - Methods add and minus for addition and subtraction of amounts of money,
+ * respectively. Both these methods should be static methods, each should have two
+ * parameters of type Money, and return a value of type Money.
+ * - A set of accessor and mutator methods to get and set values of the two instance
+ * variables.
+ * */
+public class Money {
+}
---
+++ src/tl222xy_assign2_money/MoneyMain.java
@@ -0,0 +1,23 @@
+/**
+ * MoneyMain.java
+ *
+ * @author Tetiana Lytovchenko
+ */
+package tl222xy_assign2_money;
+
+/**
+ * Write a Test program MoneyMain.java that demonstrates use of the Money class by
+ * creating two objects of type Money and uses these objects to produce following output
+ * when we run the MoneyMain.java.
+ *  Money 1 is: $2,00
+ *  Money 2 is: $5,05
+ *  Money1's dollars are: 2
+ *  Money1's cents are: 0
+ *  Money2's dollars are: 5
+ *  Money2's cents are: 5
+ *  Adding money1 and money2: $7,05
+ *  Subtracting money1 from money2: $3,05.
+ * */
+public class MoneyMain {
+
+}
