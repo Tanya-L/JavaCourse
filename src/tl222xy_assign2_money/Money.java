859d586b64e6218794b27ab74b4783c6b283f734
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-07-01 15:00:00 +0200

    added all constructors, methods and test class

--- src/tl222xy_assign2_money/Money.java
+++ src/tl222xy_assign2_money/Money.java
@@ -5,6 +5,8 @@
  */
 package tl222xy_assign2_money;
 
+import org.w3c.dom.ranges.RangeException;
+
 /**
  * Write a program Money.java that defines a class named Money whose objects represent
  * amounts of U.S. money. The class shall have:
@@ -17,6 +19,73 @@ package tl222xy_assign2_money;
  * parameters of type Money, and return a value of type Money.
  * - A set of accessor and mutator methods to get and set values of the two instance
  * variables.
- * */
+ */
 public class Money {
+
+    int dollars;
+    int cents;
+
+    // A no-argument constructor
+    public Money() {
+        dollars = 0;
+        cents = 0;
+    }
+
+    // A constructor with one parameter of type int for an amount of dollars.
+    // It should set cents to zero.
+    public Money(int dollars) {
+        this.dollars = dollars;
+        cents = 0;
+    }
+
+    // A constructor with two parameters of type int for the dollars and cents
+    public Money(int dollars, int cents) {
+        this.dollars = dollars;
+        this.cents = cents;
+    }
+
+    // Methods add and minus for addition and subtraction of amounts of money,
+    // respectively. Both these methods should be static methods, each should have two
+    // parameters of type Money, and return a value of type Money.
+    public static Money add(Money a, Money b) {
+        int aCents = a.dollars * 100 + a.cents;
+        int bCents = b.dollars * 100 + b.cents;
+        int cCents = aCents + bCents;
+        return new Money(cCents / 100, cCents % 100);
+    }
+
+    public static Money minus(Money a, Money b) {
+        int aCents = a.dollars * 100 + a.cents;
+        int bCents = b.dollars * 100 + b.cents;
+        int cCents = aCents - bCents;
+        if (cCents < 0) {
+            throw new RangeException((short) 0, "Negative money not supported");
+        }
+        return new Money(cCents / 100, cCents % 100);
+    }
+
+    @Override
+    public String toString() {
+        return "$"
+                + dollars
+                + "."
+                + (cents < 10 ? "0" : "") + cents;
+    }
+
+    // A set of accessor and mutator methods to get and set values of the two instance variables.
+    public int getDollars() {
+        return dollars;
+    }
+
+    public int getCents() {
+        return cents;
+    }
+
+    public void setDollars(int dollars) {
+        this.dollars = dollars;
+    }
+
+    public void setCents(int cents) {
+        this.cents = cents;
+    }
 }
--- src/tl222xy_assign2_money/MoneyMain.java
+++ src/tl222xy_assign2_money/MoneyMain.java
@@ -9,15 +9,32 @@ package tl222xy_assign2_money;
  * Write a Test program MoneyMain.java that demonstrates use of the Money class by
  * creating two objects of type Money and uses these objects to produce following output
  * when we run the MoneyMain.java.
- *  Money 1 is: $2,00
- *  Money 2 is: $5,05
- *  Money1's dollars are: 2
- *  Money1's cents are: 0
- *  Money2's dollars are: 5
- *  Money2's cents are: 5
- *  Adding money1 and money2: $7,05
- *  Subtracting money1 from money2: $3,05.
- * */
+ * Money 1 is: $2,00
+ * Money 2 is: $5,05
+ * Money1's dollars are: 2
+ * Money1's cents are: 0
+ * Money2's dollars are: 5
+ * Money2's cents are: 5
+ * Adding money1 and money2: $7,05
+ * Subtracting money1 from money2: $3,05.
+ */
 public class MoneyMain {
+
+    public static void main(String[] args) {
+        Money m1 = new Money(2, 0);
+        Money m2 = new Money(5, 05);
+
+        System.out.println("Money 1 is: $" + m1);
+        System.out.println("Money 2 is: $" + m2);
+
+        System.out.println("Money1's dollars are: " + m1.dollars);
+        System.out.println("Money1's cents are: " + m1.cents);
+        System.out.println("Money2's dollars are: " + m2.dollars);
+        System.out.println("Money2's cents are: " + m2.cents);
+        System.out.println("Adding money1 and money2: " + Money.add(m1, m2));
+        System.out.println("Subtracting money1 from money2: " + Money.minus(m2, m1));
+
+    }
+
 
 }
